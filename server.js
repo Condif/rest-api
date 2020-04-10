1c55f6f282f5a7a40ecc94d0c91ef57826781bfc
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-04-10 11:58:23 +0200

    basic structure

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 const express = require('express')
 const app = express()
 
+const users = [
+    {
+        name: "Victor",
+        age: 28
+    }, {
+        name: "David",
+        age: 30
+    }
+]
+
+//express.json används i varje request för att parsa
+app.use(express.json())
+
+//App tar in vad som ska hämtas via get-anrop.
+app.use(express.static('public'))
+
+app.get("/users: age", (req, res) => {
+    const foundUser = users.find((user) => {
+        if (user.age.toString() === req.params.age) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    if (!foundUser) {
+        res.status(404).send()
+    } else {
+        res.send(foundUser)
+    }
+})
