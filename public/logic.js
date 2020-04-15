4695041bc21b13e90a4b7b6f0b133e5975e9bb2e
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-04-15 13:30:53 +0200

    getSpecificShoe printSpecificShoe postShoes

--- public/logic.js
+++ public/logic.js
@@ -1,7 +1,7 @@
 // Vi kan uppnå samma sak som request.rest filen gör genom att använda fetch
 
 fetch("http://localhost:3000/shoes").then((response) => {
-   //Här måste vi parsa om bodyn till json eftersom den skickas som text från get.
+    //Här måste vi parsa om bodyn till json eftersom den skickas som text från get.
     return response.json()
     // Här hanterar vi datan och får tillbaka shoes
 }).then((shoes) => {
@@ -19,15 +19,15 @@ function printAllShoes(shoes) {
     //for loop for shoes
     shoes.forEach(shoe => {
         // Skapa html överskrift för varje egenskap och fyll innerTexten med texten i objekt med det namnet ex brand
+        let shoeId = document.createElement("h4")
+        shoeId.innerText = shoe.id
+        
         let shoeBrand = document.createElement("h4")
         shoeBrand.innerText = shoe.brand
 
-        let shoeColor = document.createElement("h4")
-        shoeColor.innerText = shoe.color
-
         let shoeSize = document.createElement("h4")
         shoeSize.innerText = shoe.size
-        
+
         let shoePrice = document.createElement("h4")
         shoePrice.innerText = shoe.price
 
@@ -38,11 +38,73 @@ function printAllShoes(shoes) {
 
         // Använd appendChild för att få med all innerText när du adderar h4:or
         shoeDiv.appendChild(shoeBrand)
-        shoeDiv.appendChild(shoeColor)
+        shoeDiv.appendChild(shoeId)
         shoeDiv.appendChild(shoeSize)
         shoeDiv.appendChild(shoePrice)
 
         //Appenda shoeDiv till allShoesContainer med dess children. 
         allShoesContainer.appendChild(shoeDiv)
     })
+}
+
+function getSpecificShoe() {
+    //Skriver vi 1 här i så fetchar vi 1 genom url sedan
+    const id = document.getElementById("shoeIdInput").value
+    
+    //Här fetchar vi shoes/ + id-numret som "användaren" skriver in
+    fetch("http://localhost:3000/shoes/" + id).then((response) => {
+        //Innan vi returnerar response objektet kan vi göra en koll för 404 och inte skicka med något för att kunna göra någt om user är undefined
+        if(response.status === 404) {
+            printSpecificShoe()
+        } else {
+            //Här måste vi parsa om bodyn till json eftersom den skickas som text från get.
+            return response.json()
+        }
+
+        
+        // Här hanterar vi datan och får tillbaka shoes
+    }).then((shoes) => {
+        console.log(shoes);
+
+        //Svaret vi får tillbaka från fetchen skickas in i printSpecificShoe
+        printSpecificShoe(shoes)
+    })
+}
+
+//Funktion som tar in shoe och skriver ut den, är shoe undefined skriver den...
+function printSpecificShoe(shoe) {
+    let specificShoeContainer = document.getElementById("specificShoe")
+    // Tömmer specificShoeContainer
+    specificShoeContainer.innerHTML = ""
+
+    // Om inte undefined gör detta
+    if(shoe) {
+         // Skapa html överskrift för varje egenskap och fyll innerTexten med texten i objekt med det namnet ex brand
+         let shoeId = document.createElement("h4")
+         shoeId.innerText = shoe.id
+         
+         let shoeBrand = document.createElement("h4")
+         shoeBrand.innerText = shoe.brand
+ 
+         let shoeSize = document.createElement("h4")
+         shoeSize.innerText = shoe.size
+ 
+         let shoePrice = document.createElement("h4")
+         shoePrice.innerText = shoe.price
+ 
+         //Appenda alla element in shoes objektet till specificShoeContainer
+         specificShoeContainer.appendChild(shoeBrand)
+         specificShoeContainer.appendChild(shoeId)
+         specificShoeContainer.appendChild(shoeSize)
+         specificShoeContainer.appendChild(shoePrice)
+
+    // Om undefined gör detta
+    } else {
+        // Gör error respons
+        let errorResponse = document.createElement("h4")
+        errorResponse.innerText = "Hittade ingen sko"
+
+        //I specificShoeContainers lägger vi svaret från errorResponse (h4:an + innerTexten "hittade ingen sko")
+        specificShoeContainer.appendChild(errorResponse) 
+    }   
 }
--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require('express')
 const app = express()
 
+//Video tid 
+
 const shoes = [
     {
+        id: 1,
         brand: "Adidas",
-        color: "White",
         size: "41",
         price: 2300
     }, {
+        id: 2,
         brand: "Nike",
-        color: "Blue",
         size: "40",
         price: 2200
     }, {
+        id: 3,
         brand: "Reebok",
-        color: "Red",
         size: "39",
         price: 2100
     }, {
+        id: 4,
         brand: "Vans",
-        color: "Green",
         size: "38",
         price: 2000
     }
 ]
+
+
 
 //express.json används i varje request för att parsa
 app.use(express.json())
@@ -31,27 +35,29 @@ app.use(express.json())
 //App tar in vad som ska hämtas via get-anrop.
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-    console.log('get /');
-    
-})
+//gör ingenting just nu?
+// app.get('/', (req, res) => {
+//     res.send('Hello world')
+// })
 
 // When request is made on /shoes(the endpoint) respond with shoes as a json string in callback function
 app.get("/shoes", (req, res) => {
     res.json(shoes)
 })
-// i /shoes:price är price också ett requirement för att gå in i funktionen då den kollar om price = en price som finns i listan shoes
-app.get("/shoes:price", (req, res) => {
+
+//Hämta user.id i /users/:userId om user.id finns i users/:userId
+// i /shoes:shoeId är id också ett requirement för att gå in i funktionen då den kollar om id = en id som finns i listan shoes
+app.get("/shoes/:shoeId", (req, res) => {
     const foundShoe = shoes.find((shoe) => {
         //Om ett index shoe har samma nummer som ett i listan som finns på servern sätt foundShoe till true annars false
-        if (user.price.toString() === req.params.price) {
+        if (shoe.id.toString() === req.params.shoeId) {
             return true
         } else {
             return false
         }
     })
 
+    //Vad svaret blir beroende på vad find funktion hittar, hittar den undefined (false) sätts rests status till 404 inget svar
     if (!foundShoe) {
         res.status(404).send()
     } else {
@@ -60,6 +66,14 @@ app.get("/shoes:price", (req, res) => {
     } 
 })
 
+app.post('/shoes', (req, res) => {
+    shoes.push(req.body)
+    res.status(201).send()
+})
+
+//Lägg till ett hostnamn + en port
 const hostname = 'localhost'
+
+// porten är det du manuellt ställt in(fungerar inte just nu) annars är den 3000 (standard)
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on   http://${hostname}:${port}`));
